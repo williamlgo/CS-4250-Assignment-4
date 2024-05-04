b336119504fb3c12ddd4e99a1497ce513d1e8bd1
William Go <wlgo@cpp.edu>
2024-05-03 21:50:35 -0700

    Updating

--- crawler.py
+++ crawler.py
@@ -6,6 +6,18 @@ import urllib as urllib
 import bs4 as bs
 from pymongo import MongoClient
 
+#Connect the Database
+def connectDataBase():
+    DB_NAME = "CPP"
+    DB_HOST = "localhost"
+    DB_PORT = "27017"
+    try:
+        client = MongoClient(host=DB_HOST, port=DB_PORT)
+        db = client[DB_NAME]
+        return db
+    except:
+        print("Database not connected successfully")
+
 #Pseudocode
 # procedure crawlerThread (frontier)
 #      while not frontier.done() do
@@ -21,18 +33,42 @@ from pymongo import MongoClient
 #      end while
 # end procedure
 
-#The frontier object?   
+#The frontier object 
 class Frontier: 
     def __init__(self, starting_url):
         self.queue = [starting_url]
         self.visited = set()
 
+    #Add urls to queue if not yet visited
+    def addURL(self, url):
+        if url not in self.visited and url not in self.queue:
+            self.queue.append(url)
+
+    #Next URL in queue (pop the previous)
+    def nextURL(self):
+        url = self.queue.pop(0)
+        self.visited.add(url)
+        return url
+
+    #Done if the queue is 0
+    def done(self):
+        return len(self.queue) == 0
+
+#Function to check if it is the actual target
+def target(html):
+    allText = bs.text
+
+
+
 #The actual crawler based on pseudocode
 def crawlerThread(frontier):
-
+    while not frontier.done():
+        url = frontier.nextURL()
 
 def main(): 
     starting_url = 'https://www.cpp.edu/sci/computer-science/'
+    db = connectDataBase()
+    professors = db.professors
     frontier = Frontier(starting_url)
     crawlerThread(frontier)
 
